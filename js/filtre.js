9a22f112abb8eaf367399877d2d6f3537395cfff
Guillaumelaurent23 <guillaume.laurent23@outlook.fr>
2016-11-22 18:17:23 +0100

    Systeme de filtre

---
+++ js/filtre.js
@@ -0,0 +1,25 @@
+/**
+ * Created by guillaume on 22/11/2016.
+ */
+
+
+(function () {
+    "use strict";
+
+    var listeImage = document.querySelectorAll('imgFiltre');
+
+
+    function basculeGris() {
+        for (var i = 0; i < listeImage.length; i++) {
+            var uneImage = listeImage[i];
+            uneImage.classList.remove('cache');
+        }
+        this.classList.add('cache');
+    }
+
+    for (var i = 0; i < listeImage.length; i++) {
+        var uneImage = listeImage[i];
+        uneImage.addEventListener("click", basculeGris);
+    }
+
+}());
--- pages/Admin/modif/suppEnt.php
+++ pages/Admin/modif/suppEnt.php
@@ -1,6 +1,6 @@
 <?php
 require_once('dao/DaoEntreprise.php');
-session_start();
+
 
 $daoEntreprise = new DaoEntreprise();
 $daoEntreprise->find($_GET["id"]);
