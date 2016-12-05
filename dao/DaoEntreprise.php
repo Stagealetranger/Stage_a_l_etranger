1ae91a67c05f43706b2e0191796c74725e00f2e8
Guillaumelaurent23 <guillaume.laurent23@outlook.fr>
2016-12-05 14:14:48 +0100

    modif

--- dao/DaoEntreprise.php
+++ dao/DaoEntreprise.php
@@ -128,7 +128,7 @@ class DaoEntreprise extends Dao
     public function setLesPersonnesVont()
     {
         $sql = "SELECT * 
-                FROM personne, est_en_stage  
+                FROM personne, est_en_stage, entreprise    
                 WHERE 
                 entreprise.ID_ENTREPRISE = est_en_stage.ID_ENTREPRISE 
                 est_en_stage.ID_PERSONNE = personne.ID_PERSONNE 
