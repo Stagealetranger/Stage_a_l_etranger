da715c8516abaf272adaadc3b7196ab9fc6bff41
Guillaumelaurent23 <guillaume.laurent23@outlook.fr>
2016-12-29 10:02:56 +0100

    remis en place

--- classes/class.OnAccueilli.php
+++ classes/class.OnAccueilli.php
@@ -5,19 +5,21 @@ require_once('class.Personne.php');
 class OnAccueilli extends Entreprise
 {
     private $description_avis = null;
-    
-    
-    private $lesEntreprise = array();
+    private $lesPersonnesDeja = array();
 
 
-    public function __construct($id=0,$nom= null ,$description_avis = null)
+    public function OnAccueilli($id = 0, $nom = null, $visiter = false, $avis = false, $taille = false, $description = null, $rue = null, $description_avis = null, $profil = null, $pays = null, $ville = null, $description_avis=0)
     {
-       parent::__construct($id, $nom);
+        // --- Appel du constructeur de la classe m�re ---
+        // Ici la classe Casque pour lequel il faut id, nom et cout
+        parent::__construct($id = 0, $nom = null, $visiter = false, $avis = false, $taille = false, $description = null, $rue = null, $description_avis = null, $profil = null, $pays = null, $ville = null);
+        // --- On compl�te ensuite la construction de l'objet
+        // --- En initialisant les attributs restants
         $this->description_avis = $description_avis;
     }
 
-
     public function getDescriptionAvis() {return $this->description_avis;}
     public function setDescriptionAvis($description_avis){$this->description_avis=$description_avis;}
-
+    public function getLesPersonnesDeja(){return $this->lesPersonnesDeja;}
+    public function setLesPersonnesDeja($lesPersonnesDeja){$this->lesPersonnesDeja = $lesPersonnesDeja;}
 }
