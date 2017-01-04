6f6583c6c3d7327fc4c26e5aab6ee71eba1af239
Jfroehly <joshua.froehly@gmail.com>
2017-01-04 17:40:15 +0100

    mail conf

--- pages/Admin/listeValidation.php
+++ pages/Admin/listeValidation.php
@@ -33,6 +33,56 @@ if (isset($_POST["accepter"])) {
     $daoPersonne->create();
     $daoValidation->delete();
     header('Location: index.php?page=listeValidation');
+    $mail = 'joshua.froehly@gmail.com'; // Déclaration de l'adresse de destination.
+    if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
+    {
+        $passage_ligne = "\r\n";
+    }
+    else
+    {
+        $passage_ligne = "\n";
+    }
+//=====Déclaration des messages au format texte et au format HTML.
+    $message_txt = "Bonjour,votre inscription a bien été validée nous vous remercions pour votre attente.";
+    $message_html = "<html><head></head><body><b>Bonjour</b>,votre inscription a bien été validée nous vous remercions pour votre attente <i>script PHP</i>.</body></html>";
+//==========
+
+
+//=====Création de la boundary.
+    $boundary = "-----=".md5(rand());
+//==========
+
+//=====Définition du sujet.
+    $sujet = "Inscription validée";
+//=========
+
+//=====Création du header de l'e-mail.
+    $header = "From: \"Stage\"<joshua.froehly@gmail.com>".$passage_ligne;
+    $header.= "Reply-to: \"Stage\" <joshua.froehly@gmail.com>".$passage_ligne;
+    $header.= "MIME-Version: 1.0".$passage_ligne;
+    $header.= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+//==========
+
+//=====Création du message.
+    $message = $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format texte.
+    $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+    $message.= $passage_ligne.$message_txt.$passage_ligne;
+//==========
+    $message.= $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format HTML
+    $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+    $message.= $passage_ligne.$message_html.$passage_ligne;
+//==========
+    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+//==========
+
+//=====Envoi de l'e-mail.
+    mail($mail,$sujet,$message,$header);
+//==========
 }
 
 if (isset($_POST["refuser"])) {
