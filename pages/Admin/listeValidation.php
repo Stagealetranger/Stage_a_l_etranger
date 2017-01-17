c112238e28a22b7f9f1677b2434abd32b50c84fc
Guillaumelaurent23 <guillaume.laurent23@outlook.fr>
2017-01-17 14:02:34 +0100

    modif +autocomplete

--- pages/Admin/listeValidation.php
+++ pages/Admin/listeValidation.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once 'dao/DaoValidation.php';
 require_once 'dao/DaoPersonne.php';
@@ -7,7 +6,7 @@ require_once 'dao/DaoPersonne.php';
 $daoValidation = new DaoValidation();
 
 
-$listeValidation = $daoValidation ->getListe();
+$listeValidation = $daoValidation->getListe();
 
 
 for ($i = 0; $i < count($listeValidation); $i++) {
@@ -37,19 +36,17 @@ if (isset($_POST["accepter"])) {
     if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
     {
         $passage_ligne = "\r\n";
-    }
-    else
-    {
+    } else {
         $passage_ligne = "\n";
     }
 //=====Déclaration des messages au format texte et au format HTML.
     $message_txt = "Bonjour,votre inscription a bien été validée nous vous remercions pour votre attente.";
-    $message_html = "<html><head></head><body><b>Bonjour</b>,votre inscription a bien été validée nous vous remercions pour votre attente <i>script PHP</i>.</body></html>";
+  
 //==========
 
 
 //=====Création de la boundary.
-    $boundary = "-----=".md5(rand());
+    $boundary = "-----=" . md5(rand());
 //==========
 
 //=====Définition du sujet.
@@ -57,31 +54,27 @@ if (isset($_POST["accepter"])) {
 //=========
 
 //=====Création du header de l'e-mail.
-    $header = "From: \"Stage\"<joshua.froehly@gmail.com>".$passage_ligne;
-    $header.= "Reply-to: \"Stage\" <joshua.froehly@gmail.com>".$passage_ligne;
-    $header.= "MIME-Version: 1.0".$passage_ligne;
-    $header.= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+    $header = "From: \"Stage\"<joshua.froehly@gmail.com>" . $passage_ligne;
+    $header .= "Reply-to: \"Stage\" <joshua.froehly@gmail.com>" . $passage_ligne;
+    $header .= "MIME-Version: 1.0" . $passage_ligne;
+    $header .= "Content-Type: multipart/mixed;" . $passage_ligne . " boundary=\"$boundary\"" . $passage_ligne;
 //==========
 
 //=====Création du message.
-    $message = $passage_ligne."--".$boundary.$passage_ligne;
+    $message = $passage_ligne . "--" . $boundary . $passage_ligne;
 //=====Ajout du message au format texte.
-    $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-    $message.= $passage_ligne.$message_txt.$passage_ligne;
+    $message .= "Content-Type: text/plain; charset=\"ISO-8859-1\"" . $passage_ligne;
+    $message .= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
+    $message .= $passage_ligne . $message_txt . $passage_ligne;
 //==========
-    $message.= $passage_ligne."--".$boundary.$passage_ligne;
-//=====Ajout du message au format HTML
-    $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-    $message.= $passage_ligne.$message_html.$passage_ligne;
+    $message .= $passage_ligne . "--" . $boundary . $passage_ligne;
 //==========
-    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+    $message .= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
+    $message .= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
 //==========
 
 //=====Envoi de l'e-mail.
-    mail($mail,$sujet,$message,$header);
+    mail($mail, $sujet, $message, $header);
 //==========
     header('Location: index.php?page=listeValidation');
 }
@@ -90,15 +83,15 @@ if (isset($_POST["refuser"])) {
     $daoValidation = new DaoValidation();
     $daoValidation->find($_GET["id"]);
     $daoValidation->delete();
-    
+
     header('Location: index.php?page=listeValidation');
 }
 
 
-if (($_SESSION['mail']) == ''){
+if (($_SESSION['mail']) == '') {
     header('Location: index.php?page=accueil');
 
-}else {
+} else {
     if ((($_SESSION['admin']) == "0")) {
         header('Location: index.php?page=Etudiant');
     }
